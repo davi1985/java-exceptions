ec8658393be85b6904c25004f13d9803842e63e7
Davi Silva <davi.psilva@midway.com.br>
2024-10-31 14:44:21 -0300

    exceptions types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        method1();
+        System.out.println("End of program...");
+    }
+
+    public static void method1() {
+        method2();
+    }
+
+    public static void method2() {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String[] text = scanner.nextLine().split(" ");
+            int position = scanner.nextInt();
+            System.out.println(text[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+            e.printStackTrace();
+
+            scanner.next();
+        } catch (InputMismatchException e) {
+            System.out.println("Input error!");
+        }
+
+        scanner.close();
     }
 }
